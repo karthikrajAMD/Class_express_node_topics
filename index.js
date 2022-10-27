158c69775477e518b22e0b6e0befc19f149eff8c
Karthik Raja <karthikrajaamd@gmail.com>
2022-10-27 15:18:42 +0530

    put added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { MongoClient } = require("mongodb");
 require("dotenv").config();
 // import { MongoClient } from "mongodb";
 // import express from "express";
-console.log(process.env);
+
 const app = express();
 
 const PORT = process.env.PORT;
@@ -14,8 +14,7 @@ const client = new MongoClient(MONGO_URL);
 // const dbName = "b35we";
 async function createConnection() {
   await client.connect();
-
-  console.log("monogo is connected");
+  console.log("monogodb is connected");
   // const db = client.db(dbName);
   // const collection = db.collection("movies");
   return "done.";
@@ -32,7 +31,7 @@ app.get("/", (req, res) => {
 // movies with id route (using request params)..............
 app.get("/movies/:id", async (req, res) => {
   const { id } = req.params;
-  console.log(id);
+  console.log("ID is:", id);
   // const movie = movies.filter((mv) => mv.id == id);
   const movie = await client
     .db("b35we")
@@ -40,7 +39,7 @@ app.get("/movies/:id", async (req, res) => {
     .findOne({ id: id });
   movie
     ? res.send(movie)
-    : res.status(404).send({ message: "Movie not found" });
+    : res.status(404).send({ message: "Movie of particular id is not found" });
 });
 // movies with query(using request query)..................
 app.get("/movies", async (req, res) => {
@@ -90,6 +89,22 @@ app.post("/movies", async (req, res) => {
   result
     ? res.send(result)
     : res.status(404).send({ message: "Nothing on body to post" });
+});
+// Put Movie..............................................
+app.put("/movies/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const updateMovie = req.body;
+  console.log(req.params);
+  const update = await client
+    .db("b35we")
+    .collection("movies")
+    .updateOne({ id: id }, { $set: updateMovie });
+
+  res.send(update);
+  // updateMovie
+  //   ? res.send(update)
+  //   : res.status(404).send({ message: "Nothing on body to update" });
 });
 //.............................................................
 app.listen(PORT, () => {
